0e946e3fd0811c70e874e1d86780989dbcae1748
YosefYuan <yuanruixiangjet@163.com>
2017-12-06 01:21:18 +0800

    1206

---
+++ 08-02search.js
@@ -0,0 +1,31 @@
+function Search() {
+    this.sequentialSearch = function (item) {
+        for (var i = 0; i < array.length; i++) {
+            if (item === array[i]) {
+                return i;
+            }
+        }
+        return -1;
+    };
+
+    this.binarySearch = function () {
+        this.quickSort();
+
+        var low = 0,
+            high = array.length - 1,
+            mid, element;
+
+        while (low <= high) {
+            mid = Math.floor((low + high) / 2);
+            element = array[mid];
+            if (element < item) {
+                low = mid + 1;
+            } else if (element > item) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
--- 08ArrayList.js
+++ 08ArrayList.js
@@ -101,7 +101,53 @@ function ArrayList() {
     };
     this.mergeSort = function () {
         array = mergeSortRec(array);
-    }
+    };
+
+    var swapQuickStort = function (array, index1, index2) {
+        var aux = array[index1];
+        array[index1] = array[index2];
+        array[index2] = aux;
+    };
+
+    var partition = function (array, left, right) {
+
+        var pivot = array[Math.floor((right + left) / 2)],
+            i = left,
+            j = right;
+
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swapQuickStort(array, i, j);
+                i++;
+                j--;
+            }
+        }
+        return i;
+    };
+
+    var quick = function (array, left, right) {
+        var index;
+        if (array.length > 1) {
+            index = partition(array, left, right);
+
+            if (left < index - 1) {
+                quick(array, left, index - 1);
+            }
+            if (index < right) {
+                quick(array, index, right);
+            }
+        }
+    };
+
+    this.quickSort = function () {
+        quick(array, 0, array.length - 1);
+    };
 }
 
 function createNonSortedArray(size) {
